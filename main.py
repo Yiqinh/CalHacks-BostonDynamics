dbccecc2666e5965032c756731b976bfd32f6c6e
Arseniy <arseniypopugay@gmail.com>
2024-10-19 02:11:55 +0300

    fix Spot address

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from spot_controller import SpotController
 import cv2
 
-ROBOT_IP = "10.0.0.3"#os.environ['ROBOT_IP']
+ROBOT_IP = "192.168.80.3"#os.environ['ROBOT_IP']
 SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
 SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
 
