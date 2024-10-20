07460e0e4a0f6b4935d0933c35d61f3abb9ec7f4
Yiqin <yiqinhuang@berkeley.edu>
2024-10-19 21:15:16 -0700

    e

--- test_spot.py
+++ test_spot.py
@@ -11,7 +11,7 @@ SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
 
 if __name__ == '__main__':
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
-        spot.move_head_in_points(yaws=[0], pitches=[0.3], rolls=[0], sleep_after_point_reached=-0.3)  # Head bob
+        spot.move_head_in_points(yaws=[0], pitches=[-0.3], rolls=[0], sleep_after_point_reached=0.3)  # Head bob
         spot.image(camera='frontright')
         print("image done")
     print("done")
