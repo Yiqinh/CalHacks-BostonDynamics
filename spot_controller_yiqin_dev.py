52368bdc625eac44b708af3c33f7efaadd81bf15
Yiqin <yiqinhuang@berkeley.edu>
2024-10-19 19:36:09 -0700

    change save location to tmp

--- spot_controller_yiqin_dev.py
+++ spot_controller_yiqin_dev.py
@@ -90,7 +90,7 @@ class SpotController:
                 out = cv2.rotate(out, cv2.ROTATE_180)
 
         # Write the image out.
-        filename = f'/home/calhacks/yiqin/CalHacks-BostonDynamics/{camera}.jpg'
+        filename = f'/tmp/{camera}.jpg'
         cv2.imwrite(filename, out)
 
 
