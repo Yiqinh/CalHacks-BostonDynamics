0c9cf292c908fa6efcd365cf1331cba615a43497
mattg747 <matthewgerardi@berkeley.edu>
2024-10-19 20:26:26 -0700

    testing out make_stance again, very basic

--- main_griddy.py
+++ main_griddy.py
@@ -20,29 +20,34 @@ def perform_griddy(controller):
     controller.stand_at_height(body_height=0.0)
     time.sleep(1)
 
+    controller.make_stance(x_offset=0.1, y_offset=0)
+    time.sleep(2)
+    controller.make_stance(x_offset=-0.1, y_offset=0)
+    time.sleep(2)
+
     # Step 2: Simulate skipping footwork using velocity control and leg lifting
-    for _ in range(4):
-        # Forward skip
-        controller.move_by_velocity_control(v_x=0.4, v_y=0.0, cmd_duration=1.5)
-        time.sleep(0.3)
+    # for _ in range(4):
+    #     # Forward skip
+    #     controller.move_by_velocity_control(v_x=0.4, v_y=0.0, cmd_duration=1.5)
+    #     time.sleep(0.3)
 
-        # Side step
-        controller.move_by_velocity_control(v_x=0.0, v_y=0.4, cmd_duration=0.5)
-        time.sleep(0.3)
+    #     # Side step
+    #     controller.move_by_velocity_control(v_x=0.0, v_y=0.4, cmd_duration=0.5)
+    #     time.sleep(0.3)
 
-        # Lift front-left leg, then back-right leg to simulate skipping
-        # controller.make_stance(x_offset=0.2, y_offset=0.1)  # Lift front-left leg
-        # time.sleep(0.3)
-        # controller.make_stance(x_offset=-0.2, y_offset=-0.1)  # Lift back-right leg
-        # time.sleep(0.3)
+    #     # Lift front-left leg, then back-right leg to simulate skipping
+    #     # controller.make_stance(x_offset=0.2, y_offset=0.1)  # Lift front-left leg
+    #     # time.sleep(0.3)
+    #     # controller.make_stance(x_offset=-0.2, y_offset=-0.1)  # Lift back-right leg
+    #     # time.sleep(0.3)
 
-    # Step 3: Head bobbing with pitch control (up and down movement)
-    for _ in range(2):
-        controller.move_head_in_points(yaws=[0, 0], pitches=[0.3, -0.3], rolls=[0, 0], sleep_after_point_reached=0.3)
-        time.sleep(1)
+    # # Step 3: Head bobbing with pitch control (up and down movement)
+    # for _ in range(2):
+    #     controller.move_head_in_points(yaws=[0, 0], pitches=[0.3, -0.3], rolls=[0, 0], sleep_after_point_reached=0.3)
+    #     time.sleep(0.3)
 
-    # Step 4: Add playful head yaw (looking side to side)
-    controller.move_head_in_points(yaws=[0.3, -0.3], pitches=[0, 0], rolls=[0, 0], sleep_after_point_reached=0.4)
+    # # Step 4: Add playful head yaw (looking side to side)
+    # controller.move_head_in_points(yaws=[0.3, -0.3], pitches=[0, 0], rolls=[0, 0], sleep_after_point_reached=0.4)
 
     # Step 5: End with a stance reset to neutral
     controller.stand_at_height(body_height=0.0)
